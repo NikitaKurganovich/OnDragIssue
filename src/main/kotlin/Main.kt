bc4ff9a09be768ceec5b1c9abe8781be77795fde
Babananick <babananick@gmail.com>
2023-10-09 20:28:06 +0300

    Made a working demonstration of the bug

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,7 +26,7 @@ fun main() = application {
         /*
             This is a demonstration of a bug with FilePicker function by Wavesonics
             If you try to drag elements out of dialog in MP file picker
-            after opening drag and drop screen, application will be frozen constantly.
+            after opening drag and drop screen, application will be frozen.
             AWT file picker work fine in this situation.
             This problem appears only after opening drag and drop window.
             You have only open drag and drop screen to reproduce this bug
